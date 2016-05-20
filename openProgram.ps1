e364abd4a4439e6060eda4c2a11332de6e747298
Kristinn Kristmundsson <kristinnes@gmail.com>
2016-05-20 23:27:38 +0000

    Finished adding a working version to resize explorer

--- openProgram.ps1
+++ openProgram.ps1
@@ -1,19 +1,21 @@
 ipmo .\UIAutomation\UIAutomation.dll
 
-Hide-UiaCurrentHighlighter
+
 
 Start-Process 'C:\Program Files (x86)\Sublime Text 3\sublime_text.exe'
 $sublime = Get-UIAWindow -ProcessName sublime_text | Invoke-UIAWindowTransformResize -TransformResizeWidth 2560 -TransformResizeHeight 695;
-$sublime.Move(0, 720);
+$sublime.Move(0, 718);
 
 Start-Process chrome
 $chrome = Get-UIAWindow -ProcessName chrome | Invoke-UIAWindowTransformResize -TransformResizeWidth 1920 -TransformResizeHeight 1080;
 $chrome.Move(-1920, 846);
 
 Start-Process explorer C:\development
+Start-Sleep -s 2
 $expl = Get-UIAWindow -ProcessName explorer | Invoke-UIAWindowTransformResize -TransformResizeWidth 1280 -TransformResizeHeight 720 | Invoke-UiaWindowTransformMove -TransformMoveX 0 -TransformMoveY 0;
-$expl.Move(-1920, 846);
 
 Start-Process powershell
 $powersh = Get-UIAWindow -ProcessName powershell | Invoke-UIAWindowTransformResize -TransformResizeWidth 1280 -TransformResizeHeight 720;
 $powersh.Move(1280, 0);
+
+Hide-UiaCurrentHighlighter
